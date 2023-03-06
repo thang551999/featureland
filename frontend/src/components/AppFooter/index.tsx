ea99a8ea502d387881f4d78086ec2e7b2178db71
bahoang3105 <73835461+bahoang3105@users.noreply.github.com>
2023-03-06 21:09:32 +0700

    Add header, footer

--- frontend/src/components/AppFooter/index.tsx
+++ frontend/src/components/AppFooter/index.tsx
@@ -1,7 +1,30 @@
+import { Row, Col } from "antd"
+import Link from "next/link"
+
 export default function AppFooter() {
   return (
-    <div>
-      
+    <div className="footer">
+      <div className="footer__content">
+        <Row className="footer__content__links">
+          <Col span={4} className="footer__content__links__logo"><img src="/svg/LOGO.svg"></img></Col>
+          <Col></Col>
+          <Col span={12} className="footer__content__links__pages">
+            <Link href="https://dribbble.com/"><span>Home</span></Link>
+            <Link href="https://instagram.com/"><span>About Us</span></Link>
+            <Link href="https://www.linkedin.com"><span>Pricing</span></Link>
+            <Link href="https://www.linkedin.com"><span>Features</span></Link>
+            <Link href="https://www.linkedin.com"><span>Contact</span></Link>
+          </Col>
+          <Col span={2}></Col>
+          <Col span={4} className="footer__content__links__social-apps">
+            <Link href="https://dribbble.com/"><img src="/svg/icon_dribbble.svg"></img></Link>
+            <Link href="https://instagram.com/"><img src="/svg/icon_instagram.svg"></img></Link>
+            <Link href="https://www.linkedin.com"><img src="/svg/icon_linkedin.svg"></img></Link>
+          </Col>
+        </Row>
+        <Row className="footer__content__license" align="middle">© 2023 DREAM LAND. All Rights Reserved.</Row>
+
+      </div>
     </div>
   )
 }
--- frontend/src/components/AppHeader/index.tsx
+++ frontend/src/components/AppHeader/index.tsx
@@ -4,6 +4,7 @@ import AppConnectWalletButton from '@/components/AppConnectWalletButton';
 import { logout } from '@/redux/authentication/slice';
 import { useAppDispatch } from '@/hooks/useStore';
 import { shortenString } from '@/utils/helper';
+import Link from "next/link"
 
 export default function AppHeader() {
   const { active, account, deactivate } = useWeb3React();
@@ -22,7 +23,15 @@ export default function AppHeader() {
 
   return (
     <Row className="app-header">
-      <Col>
+      <Col span={5} className="app-header__logo"><img src="/svg/LOGO.svg"></img></Col>
+      <Col span={14} className="app-header__pages">
+        <Link href="https://dribbble.com/"><span>Home</span></Link>
+        <Link href="https://instagram.com/"><span>Community</span></Link>
+        <Link href="https://www.linkedin.com"><span>Our Teams</span></Link>
+        <Link href="https://www.linkedin.com"><span>Features</span></Link>
+        <Link href="https://www.linkedin.com"><span>Contact Us</span></Link>
+      </Col>
+      <Col span={5} className="app-header__button">
         {active && account ? (
           <Popover trigger="click" content={content}>
             <div className="app-header__account">{shortenString(account)}</div>
